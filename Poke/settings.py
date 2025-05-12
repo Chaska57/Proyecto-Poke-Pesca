a2a9d54fe5e566969ebce248ecf75411fab6e9fa
Josue Espinoza <josueespinozaurzua@gmail.com>
2025-05-12 19:32:54 -0400

    aws 2

--- Poke/settings.py
+++ Poke/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-$y#zsjfq@ct-69!l&&ohu%uew3em)xvx_&2h0ow$(##)jhr4^n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
